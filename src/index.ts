5cef78c4eb8da005f1959d6f27debdb91bef26f0
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2024-08-11 20:05:46 +0530

    add basic bot code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello World");
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+import "dotenv/config";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.MessageCreate, (message) => {
+  console.log(`Recieved message: [${message.author.tag}] ${message.content}`);
+  if (message.content.toLowerCase() === "ping") return message.reply("Pong!");
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
