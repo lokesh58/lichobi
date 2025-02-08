8fd43637bbe986b9ca095c42bef4dd302d8c6371
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-02-08 13:45:46 +0530

    refactor: Streamline code execution handling by extracting code block logic and enhancing modal input functionality

--- src/commands/runcode.ts
+++ src/commands/runcode.ts
@@ -1,16 +1,19 @@
 import { LichobiCommand } from "#lichobi/framework";
-import { CodeRunner, CodeRunnerParams } from "#root/utils/codeRunner.js";
+import { CodeRunner } from "#root/utils/codeRunner.js";
 import {
   ActionRowBuilder,
-  APIEmbed,
   EmbedBuilder,
-  JSONEncodable,
   Message,
   MessageContextMenuCommandInteraction,
   ModalBuilder,
   TextInputBuilder,
   TextInputStyle,
 } from "discord.js";
+
+type CodeBlock = {
+  language: string;
+  code: string;
+};
 
 export default class RuncodeCommand extends LichobiCommand(
   LichobiCommand.Base({
@@ -20,27 +23,55 @@ export default class RuncodeCommand extends LichobiCommand(
   LichobiCommand.MessageContextMenuCommandMixin(),
   LichobiCommand.LegacyMessageCommandMixin(),
 ) {
+  private static readonly InputElementId: string = "programInput";
+
   public override async handleMessageContext(
     interaction: MessageContextMenuCommandInteraction,
   ): Promise<void> {
-    const params = RuncodeCommand.extractCodeRunnerParams(
+    const codeBlock = RuncodeCommand.extractCodeBlock(
       interaction.targetMessage.content,
     );
-    if (!params) {
+    if (!codeBlock) {
       await interaction.reply({
-        embeds: [this.getInvalidCodeBlockEmbed()],
+        embeds: [RuncodeCommand.getInvalidCodeBlockEmbed()],
         ephemeral: true,
       });
       return;
     }
 
     const modalCustomId = `codeInput-${interaction.id}`;
+    const modal = RuncodeCommand.createInputModal(modalCustomId);
+    await interaction.showModal(modal);
+
+    const modalResponse = await interaction
+      .awaitModalSubmit({
+        time: 120_000,
+        filter: (i) => i.customId === modalCustomId,
+      })
+      .catch((reason) => this.bot.logger.error(reason));
+
+    if (!modalResponse) {
+      return;
+    }
+
+    await modalResponse.deferReply();
+    const input = modalResponse.fields.getTextInputValue(
+      RuncodeCommand.InputElementId,
+    );
+    const responseEmbed = await RuncodeCommand.generateResponseEmbed(
+      codeBlock,
+      input,
+    );
+    await modalResponse.editReply({ embeds: [responseEmbed] });
+  }
+
+  private static createInputModal(modalCustomId: string): ModalBuilder {
     const modal = new ModalBuilder()
       .setCustomId(modalCustomId)
       .setTitle("Program Input");
 
     const inputField = new TextInputBuilder()
-      .setCustomId("programInput")
+      .setCustomId(RuncodeCommand.InputElementId)
       .setLabel("Enter input for the program (optional)")
       .setStyle(TextInputStyle.Paragraph)
       .setRequired(false);
@@ -50,65 +81,22 @@ export default class RuncodeCommand extends LichobiCommand(
     );
     modal.addComponents(row);
 
-    await interaction.showModal(modal);
-
-    const modalResponse = await interaction
-      .awaitModalSubmit({
-        time: 120000,
-        filter: (i) => i.customId === modalCustomId,
-      })
-      .catch((err) => this.bot.logger.error(err));
-
-    if (!modalResponse) return;
-
-    await modalResponse.deferReply();
-    const input = modalResponse.fields.getTextInputValue("programInput");
-    const responseEmbed = await this.generateResponseEmbed(
-      interaction.targetMessage.content,
-      input,
-    );
-    await modalResponse.editReply({ embeds: [responseEmbed] });
+    return modal;
   }
 
   public override async handleLegacyMessage(message: Message): Promise<void> {
-    const responseEmbed = await this.generateResponseEmbed(message.content);
+    const codeBlock = RuncodeCommand.extractCodeBlock(message.content);
+    if (!codeBlock) {
+      await message.reply({
+        embeds: [RuncodeCommand.getInvalidCodeBlockEmbed()],
+      });
+      return;
+    }
+    const responseEmbed = await RuncodeCommand.generateResponseEmbed(codeBlock);
     await message.reply({ embeds: [responseEmbed] });
   }
 
-  private getInvalidCodeBlockEmbed(): EmbedBuilder {
-    return new EmbedBuilder()
-      .setTitle("Invalid code block!")
-      .setDescription("The message does not contain a valid code block.")
-      .setColor("Red");
-  }
-
-  private async generateResponseEmbed(
-    messageContent: string,
-    input?: string,
-  ): Promise<JSONEncodable<APIEmbed>> {
-    const params = RuncodeCommand.extractCodeRunnerParams(messageContent);
-    if (!params) {
-      return this.getInvalidCodeBlockEmbed();
-    }
-    if (input) params.input = input;
-
-    const codeRunner = CodeRunner.getInstance();
-    const { output, error } = await codeRunner.runCode(params);
-    return new EmbedBuilder()
-      .setTitle("Code Runner Result")
-      .addFields(
-        [
-          input ? { name: "Input", value: input } : null,
-          { name: "Output", value: output || "No output" },
-          { name: "Error", value: error || "No error" },
-        ].filter((x) => !!x),
-      )
-      .setColor("Green");
-  }
-
-  private static extractCodeRunnerParams(
-    messageContent: string,
-  ): CodeRunnerParams | null {
+  private static extractCodeBlock(messageContent: string): CodeBlock | null {
     const [res] = messageContent.matchAll(
       /(?<!\\)(```)(?<=```)(?:([a-z][a-z0-9]*)\s)(.*?)(?<!\\)(?=```)((?:\\\\)*```)/gs,
     );
@@ -117,5 +105,34 @@ export default class RuncodeCommand extends LichobiCommand(
       language: res[2],
       code: res[3],
     };
+  }
+
+  private static getInvalidCodeBlockEmbed(): EmbedBuilder {
+    return new EmbedBuilder()
+      .setTitle("Invalid code block!")
+      .setDescription("The message does not contain a valid code block.")
+      .setColor("Red");
+  }
+
+  private static async generateResponseEmbed(
+    codeBlock: CodeBlock,
+    input?: string,
+  ): Promise<EmbedBuilder> {
+    const codeRunner = CodeRunner.getInstance();
+    const { output, error } = await codeRunner.runCode({
+      language: codeBlock.language,
+      code: codeBlock.code,
+      input: input || "",
+    });
+    return new EmbedBuilder()
+      .setTitle("Code Runner Result")
+      .addFields(
+        [
+          { name: "Input", value: input || "No input" },
+          { name: "Output", value: output || "No output" },
+          { name: "Error", value: error || "No error" },
+        ].filter((x) => !!x),
+      )
+      .setColor("Green");
   }
 }
