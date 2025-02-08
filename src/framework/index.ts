7726b60c17e19cd6d94fee5fd9134abc62799d94
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-02-08 21:41:45 +0530

    fix: Restore logger export in framework index for improved logging functionality

--- src/framework/index.ts
+++ src/framework/index.ts
@@ -1,5 +1,5 @@
 export * from "./bot.js";
 export { LichobiCommand } from "./command/index.js";
 export * from "./errors.js";
-export * from "./logger.js";
 export { LichobiEvent } from "./event/index.js";
+export * from "./logger.js";
